3a09c5444f7024b367ec3b0e1a93a04e8e769ec0
Ajinkya Devasthali <ajex13@gmail.com>
2019-08-13 22:51:47 +0530

    add sign in form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 const HatsPage = () => {
   return (
@@ -24,6 +25,7 @@ function App() {
         <Route exact path="/" component={Homepage} />
         <Route exact path="/shop" component={ShopPage} />
         <Route path="/hats" component={HatsPage} />
+        <Route path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
