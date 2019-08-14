81ed00f949b1d1f231cca71d35d51510225ae44e
Ajinkya Devasthali <ajex13@gmail.com>
2019-08-14 23:04:12 +0530

    ✅convert App component to container     ✅ style fixes     ✅sign out     ✅firebase auth     ✅unsbscribe auth on unmount

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import './App.css';
-import Homepage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
-import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {auth} from './firebase/firebase.utils';
+import "./App.css";
+import Homepage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import { auth } from "./firebase/firebase.utils";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route exact path="/shop" component={ShopPage} />
-        <Route path="/hats" component={HatsPage} />
-        <Route path="/signin" component={SignInAndSignUp} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null 
+
+  componentDidMount(){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user});
+      console.log(user);
+    });
+    // console.log(this.unsubscribeFromAuth);
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser}/>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/shop" component={ShopPage} />
+          {/* <Route path="/hats" component={HatsPage} /> */}
+          <Route path="/signin" component={SignInAndSignUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,61 +1,61 @@
-import React from 'react';
-import './sign-in.styles.scss';
-import FormInput from '../form-input/form-input.component';
-import CustomButton from '../custom-button/custom-button.component';
-import {SignInWithGoogle} from '../../firebase/firebase.utils';
+import React from "react";
+import "./sign-in.styles.scss";
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
+import { SignInWithGoogle } from "../../firebase/firebase.utils";
 
 class SignIn extends React.Component {
-    constructor(props){
-        super(props);
-        this.state={
-            email:'',
-            password:''
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
 
-    handleSubmit = e => {
-        e.preventDefault()
-        this.setState({email:'', password:''})
-    }
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({ email: "", password: "" });
+  };
 
-    handleChange = e => {
-        const {value, name} = e.target;
-        this.setState({[name]:value})
-    }
+  handleChange = e => {
+    const { value, name } = e.target;
+    this.setState({ [name]: value });
+  };
 
-    render(){
-        return (
-          <div className="sign-in">
-            <h2>I already have an account</h2>
-            <span>sign in with your email and password</span>
-            <form onSubmit={this.handleSubmit}>
-              <FormInput
-                type="email"
-                name="email"
-                value={this.state.email}
-                handleChange={this.handleChange}
-                label="Email"
-                required
-              />
-              <FormInput
-                type="password"
-                name="password"
-                value={this.state.password}
-                handleChange={this.handleChange}
-                label="Password"
-                required
-              />
-              <CustomButton type="submit">
-                Submit Form
-              </CustomButton>
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>sign in with your email and password</span>
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            type="email"
+            name="email"
+            value={this.state.email}
+            handleChange={this.handleChange}
+            label="Email"
+            required
+          />
+          <FormInput
+            type="password"
+            name="password"
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label="Password"
+            required
+          />
+          <div className="buttons">
+            <CustomButton type="submit">Submit Form</CustomButton>
 
-              <CustomButton onClick={SignInWithGoogle}>
-                  Sign in with Google
-              </CustomButton>   
-            </form>
+            <CustomButton isGoogleButton onClick={SignInWithGoogle}>
+              Sign in with Google
+            </CustomButton>
           </div>
-        );
-    }
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SignIn;
