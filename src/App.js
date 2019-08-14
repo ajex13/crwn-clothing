4c3429291d702dabb57d41c23bb31efd6287155e
Ajinkya Devasthali <ajex13@gmail.com>
2019-08-14 20:57:15 +0530

    remove unwanted code

--- src/App.js
+++ src/App.js
@@ -6,16 +6,7 @@ import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>
-        HATS PAGE
-      </h1>
-    </div>
-  )
-}
+import {auth} from './firebase/firebase.utils';
 
 function App() {
   return (
